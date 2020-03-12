f4a8cd16d8f29e67591d98187a71281fa6ad4862
Paul Callaghan <p.madden90@gmail.com>
2020-03-12 11:53:06 +0000

    Removed login function temporarily

--- app.py
+++ app.py
@@ -37,15 +37,15 @@ def homepage_index():
 
 @app.route('/user/login', methods = ["POST", "GET"])
 def login(): 
-    if request.method == "POST":
-        user = request.form["nm"]
-        session["user"] = user
-        return redirect(url_for("user"))
-    else:
-        if "user" in session
-            return redirect (url_for('user'))
+    ###if request.method == "POST":
+        ###user = request.form["nm"]
+        ###session["user"] = user
+        ###return redirect(url_for("user"))
+    ###else:
+        ###if 'user' in session
+           ### return redirect (url_for('user'))
 
-        return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/user')
 def user():
