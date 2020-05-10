d20d74bf87119d514c8b7cededa28d4cb5c10fd8
Paul Callaghan <p.madden90@gmail.com>
2020-05-10 17:43:14 +0000

    Added profile.html

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ from flask_pymongo import PyMongo, pymongo
 from flask_bootstrap import Bootstrap
 from flask_paginate import Pagination, get_page_parameter, get_page_args
 import bcrypt
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
     import env
-from forms import RegistrationForm, LoginForm
-from models import User
-    
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# -------------------- #
+#      App Config      #
+# -------------------- #
 
 app = Flask(__name__)
+
 # -------------------- #
 #    DB Collections    #
 # -------------------- #
@@ -25,7 +27,8 @@ app.config["MONGO_URI"] = os.getenv("MONGO_PM_MONGO")
 app.secret_key = os.getenv("SECRET_KEY")
 usernames_collection = "users"
 mongo = PyMongo(app)
-login_manager = LoginManager(app)
+users = mongo.db.users
+
 # -------------------- #
 #        Routes        #
 # -------------------- #
@@ -42,20 +45,87 @@ def user_home():
         [{'$sample': {'size': 3 }}]
         ))
 
+# -------------------- #
+# User Authentication  #
+# -------------------- #
+
 #Login/Register
 @app.route('/user/signup', methods=["GET", "POST"])
 def signup():
-    if current_user.is_authenticated:
-        return redirect(url_for('user_home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        mongo.session.add(user)
-        mongo.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('signup.html', title='Login', form=form)
+    # Check if user is not logged in already
+	if 'user' in session:
+		flash('You are already sign in!')
+		return redirect(url_for('user_home'))
+	if request.method == 'POST':
+		form = request.form.to_dict()
+		# Check if the password and password1 actualy match 
+		if form['user_password'] == form['user_password1']:
+			# If so try to find the user in db
+			user = usernames_collection.find_one({"username" : form['username']})
+			if user:
+				flash(f"{form['username']} already exists!")
+				return redirect(url_for('register'))
+			# If user does not exist register new user
+			else:				
+				# Hash password
+				hash_pass = generate_password_hash(form['user_password'])
+				#Create new user with hashed password
+				usernames_collection.insert_one(
+					{
+						'username': form['username'],
+						'email': form['email'],
+						'password': hash_pass
+					}
+				)
+				# Check if user is actualy saved
+				user_in_db = usernames_collection.find_one({"username": form['username']})
+				if user_in_db:
+					# Log user in (add to session)
+					session['user'] = user_in_db['username']
+					return redirect(url_for('profile', user=user_in_db['username']))
+				else:
+					flash("There was a problem savaing your profile")
+					return redirect(url_for('signup'))
+
+		else:
+			flash("Passwords dont match!")
+			return redirect(url_for('signup'))
+		
+	return render_template("signup.html")
+
+# Log out
+@app.route('/logout')
+def logout():
+	# Clear the session
+	session.clear()
+	flash('You were logged out!')
+	return redirect(url_for('index'))
+
+# Profile Page
+@app.route('/profile/<user>')
+def profile(user): 
+	# Check if user is logged in
+	if 'user' in session:
+		# If so get the user and pass him to template for now
+		user_in_db = usernames_collection.find_one({"username": user})
+		return render_template('profile.html', user=user_in_db)
+	else:
+		flash("You must be logged in!")
+		return redirect(url_for('index'))
+
+# Admin area
+@app.route('/admin')
+def admin():
+	if 'user' in session:
+		if session['user'] == "admin":
+			return render_template('admin.html')
+		else:
+			flash('Only Admins can access this page!')
+			return redirect(url_for('index'))
+	else:
+		flash('You must be logged')
+		return redirect(url_for('index'))
+
         
     
 
@@ -64,44 +134,45 @@ def login_page():
     users = mongo.db.users
     return render_template('login.html')
 
-@app.route('/user/login', methods = ["GET", "POST"])
+@app.route('/user/login', methods = ["GET"])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('user_home'))
-    #if login_user:
-        #if bcrypt.haspw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-         #   session['username'] = request.form['username']
-          #  return redirect(url_for("user_home"))
-        form = LoginForm()
-        if form.validate_on_submit():
-            the_user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            return redirect(url_for("user_home"))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-        return render_template('login.html', title='Login', form=form)
+	# Check if user is not logged in already
+	if 'user' in session:
+		user_in_db = usernames_collection.find_one({"username": session['user']})
+		if user_in_db:
+			# If so redirect user to his profile
+			flash("You are logged in already!")
+			return redirect(url_for('profile', user=user_in_db['username']))
+	else:
+		# Render the page for user to be able to log in
+            return render_template('login.html')
 
 
-        
-        # put the user in 'session'
-        #session["user"] = request.form.get("new_username").lower()
-        #return redirect(url_for("users.profile", username=session["user"]))
-#Users
-# Find username in lowercase 
-def username_lower(user_lower):
-    return usernames_collection.find_one({"username_lower": user_lower.lower()})
+# Check user login details from login form
+@app.route('/user_auth', methods=['POST'])
+def user_auth():
+	form = request.form.to_dict()
+	user_in_db = usernames_collection.find_one({"username": form['username']})
+	# Check for user in database
+	if user_in_db:
+		# If passwords match (hashed / real password)
+		if check_password_hash(user_in_db['password'], form['user_password']):
+			# Log user in (add to session)
+			session['user'] = form['username']
+			# If the user is admin redirect him to admin area
+			if session['user'] == "admin":
+				return redirect(url_for('admin'))
+			else:
+				flash("You were logged in!")
+				return redirect(url_for('profile', user=user_in_db['username']))
+			
+		else:
+			flash("Wrong password or user name!")
+			return redirect(url_for('login'))
+	else:
+		flash("You must be registered!")
+		return redirect(url_for('register'))
 
-@app.route('/user')
-def user():
-    if "user" in session:
-        user = session["user"]
-        return f"<h1>{{user}}</h1>"
-
-@app.route('/user/logout')
-def logout():
-    session.pop("user", None)
-    return redirect(url_for('login'))  
 
 #Pagination
 def paginate_recipes(offset=0, per_page=6):
